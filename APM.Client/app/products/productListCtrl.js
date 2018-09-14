2eb6e97e64c7b2bbe7c9a760dc829ae74605aa3b
Andrii Turianskyi <aturyansky33@gmail.com>
2018-09-14 15:04:30 +0300

    Add project files.

---
+++ APM.Client/app/products/productListCtrl.js
@@ -0,0 +1,17 @@
+﻿(function() {
+    "use strict";
+
+   angular
+        .module("productManagement")
+        .controller("ProductListCtrl", [ "productResource",
+            productListCtrl]);
+
+    function productListCtrl(productResource) {
+        var vm = this;
+
+        productResource.query(function(data) {
+            vm.products = data;
+        });
+
+    }
+})();
---
+++ APM.WebAPI/Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using APM.WebAPI.Models;
+
+namespace APM.WebAPI.Controllers
+{
+   // [EnableCors("http://localhost:56015", "*", "*")]
+    public class ProductController : ApiController
+    {
+        // GET: api/Product
+        public IEnumerable<Product> Get()
+        {
+            ProductRepository repository  = new ProductRepository();
+
+            return repository.Retrieve();
+        }
+
+        // GET: api/Product/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Product
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Product/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Product/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
